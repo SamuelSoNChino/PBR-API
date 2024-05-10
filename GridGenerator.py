acb6d0cfd5e9e9351565269325cfa3dfb6712494
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-05-10 10:01:21 +0200

    added gitignore, added cirlce grid generator

--- GridGenerator.py
+++ GridGenerator.py
@@ -7,21 +7,42 @@ class GridGenerator:
         self.image_size = image_size
         self.pieces = pieces
         self.grid = None
+        self.circle_size = 5
+        self.background_color = (255, 255, 255)
+        self.grid_color = (0, 0, 0)
 
     def set_parameters(self, image_size: int, pieces: int) -> "GridGenerator":
         self.image_size = image_size
         self.pieces = pieces
         return self
 
-    def generate_grid(self) -> "GridGenerator":
-        grid = np.ones(
-            (self.image_size, self.image_size, 3), dtype=np.uint8)
+    def generate_line_grid(self) -> "GridGenerator":
+        grid = np.ndarray((self.image_size, self.image_size, 3))
+        grid[:, :] = self.background_color
+
         for i in range(self.pieces + 1):
             pos = (i * self.image_size) // self.pieces
             if pos == self.image_size:  # So that the last line is still in the image
                 pos -= 1
-            cv.line(grid, (pos, 0), (pos, self.image_size), (255, 255, 255), 1)
-            cv.line(grid, (0, pos), (self.image_size, pos), (255, 255, 255), 1)
+            cv.line(grid, (pos, 0), (pos, self.image_size), self.grid_color, 1)
+            cv.line(grid, (0, pos), (self.image_size, pos), self.grid_color, 1)
+
+        self.grid = grid
+        return self
+
+    def generate_circle_grid(self) -> "GridGenerator":
+        grid = np.ndarray((self.image_size, self.image_size, 3))
+        grid[:, :] = self.background_color
+
+        radius = int(self.image_size / self.pieces / self.circle_size)
+        for i in range(self.pieces):
+            y = int((i * self.image_size) / self.pieces +
+                    self.circle_size / 2 * radius)
+            for j in range(self.pieces):
+                x = int((j * self.image_size) / self.pieces +
+                        self.circle_size / 2 * radius)
+                cv.circle(grid, (y, x), radius, self.grid_color, 5)
+
         self.grid = grid
         return self
 
--- test.py
+++ test.py
@@ -5,10 +5,9 @@ import cv2 as cv
 tiles = 4
 image_size = 1000
 image = GeoShapesGenerator(image_size, tiles).generate_image().get_image()
-grid = GridGenerator(image_size, tiles).generate_grid().get_grid()
-image = cv.bitwise_or(grid, image)
+grid = GridGenerator(image_size, tiles).generate_circle_grid().get_grid()
 cv.imwrite("puzzle.png", image)
 cv.imwrite("grid.png", grid)
-cv.imshow("Image", cv.bitwise_or(grid, image))
+cv.imshow("Image", grid)
 cv.waitKey(0)
 ()
