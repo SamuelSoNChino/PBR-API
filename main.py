dccebeb4ec88a112f56691a812fed5cb0adfdc93
SamuelSoNChino <164644134+SamuelSoNChino@users.noreply.github.com>
2024-05-10 11:26:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, send_file, request
 from GeoShapesGenerator import GeoShapesGenerator
+from GridGenerator import GridGenerator
 import cv2 as cv
 import io
 
@@ -14,6 +15,16 @@ def generate_image():
     _, image_encoded = cv.imencode("image.png", image)
     image_bit_array = io.BytesIO(image_encoded)
     return send_file(image_bit_array, "image/png")
+
+
+@app.route("/generate_grid")
+def generate_grid():
+    image_size = int(request.args.get("image_size"))
+    pieces = int(request.args.get("pieces"))
+    grid = GridGenerator(image_size, pieces).generate_circle_grid().get_grid()
+    _, grid_encoded = cv.imencode("image.png", grid)
+    grid_bit_array = io.BytesIO(grid_encoded)
+    return send_file(grid_bit_array, "grid/png")
 
 
 if __name__ == '__main__':
